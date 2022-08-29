db209cfc7406cb21fbcaadeac9efd5c927016c39
Cole <cajen414@learner.dvusd.org>
2022-08-29 09:21:11 -0700

    monday
    hi

---
+++ C#/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nametextbox = new System.Windows.Forms.TextBox();
+            this.cctextbox = new System.Windows.Forms.TextBox();
+            this.exptextbox = new System.Windows.Forms.TextBox();
+            this.ziptextbox = new System.Windows.Forms.TextBox();
+            this.amounttextbox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 289);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 289);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CC#";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Exp Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 116);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Zip";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Amount:";
+            // 
+            // nametextbox
+            // 
+            this.nametextbox.Location = new System.Drawing.Point(88, 4);
+            this.nametextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nametextbox.Name = "nametextbox";
+            this.nametextbox.Size = new System.Drawing.Size(132, 22);
+            this.nametextbox.TabIndex = 7;
+            // 
+            // cctextbox
+            // 
+            this.cctextbox.Location = new System.Drawing.Point(88, 36);
+            this.cctextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cctextbox.Name = "cctextbox";
+            this.cctextbox.Size = new System.Drawing.Size(132, 22);
+            this.cctextbox.TabIndex = 8;
+            // 
+            // exptextbox
+            // 
+            this.exptextbox.Location = new System.Drawing.Point(88, 75);
+            this.exptextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exptextbox.Name = "exptextbox";
+            this.exptextbox.Size = new System.Drawing.Size(132, 22);
+            this.exptextbox.TabIndex = 9;
+            // 
+            // ziptextbox
+            // 
+            this.ziptextbox.Location = new System.Drawing.Point(88, 112);
+            this.ziptextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ziptextbox.Name = "ziptextbox";
+            this.ziptextbox.Size = new System.Drawing.Size(132, 22);
+            this.ziptextbox.TabIndex = 10;
+            this.ziptextbox.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // amounttextbox
+            // 
+            this.amounttextbox.Location = new System.Drawing.Point(88, 149);
+            this.amounttextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.amounttextbox.Name = "amounttextbox";
+            this.amounttextbox.Size = new System.Drawing.Size(132, 22);
+            this.amounttextbox.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.master;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 340);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Fee";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 554);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.amounttextbox);
+            this.Controls.Add(this.ziptextbox);
+            this.Controls.Add(this.exptextbox);
+            this.Controls.Add(this.cctextbox);
+            this.Controls.Add(this.nametextbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Name";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nametextbox;
+        private System.Windows.Forms.TextBox cctextbox;
+        private System.Windows.Forms.TextBox exptextbox;
+        private System.Windows.Forms.TextBox ziptextbox;
+        private System.Windows.Forms.TextBox amounttextbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ C#/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double amt;
+            double.TryParse(textBox1.Text, out amt);
+            textBox2.Text = (amt * 0.050).ToString("c2");
+            double.TryParse(textBox1.Text, out amt);
+            textBox3.Text = (amt * 1.00).ToString("c2");
+            double.TryParse(textBox1.Text, out amt);
+            textBox4.Text = (amt * .20).ToString("c2");
+            double.TryParse(textBox1.Text, out amt);
+            textBox5.Text = (amt * .51).ToString("c2");
+            double.TryParse(textBox1.Text, out amt);
+            textBox6.Text = (amt * .71).ToString("c2");
+            double.TryParse(textBox1.Text, out amt);
+            textBox7.Text = (amt * .04).ToString("c2");
+        }
+    }
+}
